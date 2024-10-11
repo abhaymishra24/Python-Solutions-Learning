e0fbe68c622f008181206b8f98f5dc149f862e47
Abhay Mishra <techabhay06@gmail.com>
2024-10-12 02:46:21 +0530

    Add new commit

--- Python_solutions/function_problem1.py
+++ Python_solutions/function_problem1.py
@@ -1,17 +1,2 @@
 
-# we will learn function in this file
-
-# we write a first programm in function
-
-# lets print hello function era by function method
-
-
-def func1():      # this is function definition    
-
-    print("Hello Function Era:")
-    print("Welcome, I will try my best to learn function in python.")
-
-
-func1()   # this is function call
-
-# The sentx of function programm 
+# 
