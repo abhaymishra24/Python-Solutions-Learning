8269b4cea9192fc98274471872d1a95cc4845ce2
Abhay Mishra <techabhay06@gmail.com>
2024-10-14 17:10:26 +0530

    Add new commit

--- Python_solutions/first.py
+++ Python_solutions/first.py
@@ -3,4 +3,20 @@
 
 # Here we write our first programm in python for print statment
 
+# this is normel print statment
+
 print("Hello World")
+
+# here we solve print statment with varibles
+
+a = 24            # we can give varible name here 
+
+print(a)              # print the varible value
+
+# here we can give some word form varible as well
+
+a = "Hello world, This python programming"
+
+print(a)                                      # same as print value
+
+ 
