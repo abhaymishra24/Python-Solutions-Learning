f3780f373234fd4e35e2c0571b1fcae00fa0bead
Abhay Mishra <techabhay06@gmail.com>
2024-10-14 16:32:40 +0530

    Add new commit

--- Python_solutions/function_problem4.py
+++ Python_solutions/function_problem4.py
@@ -1,2 +1,11 @@
 
-# 
+# write a solution to find out sum of n number using recursive 
+
+def sum(n):          # difine value
+    if(n==1):                    # base condition 
+        return 1
+    
+    return sum(n-1) + n                  # write a logic
+
+n = int(input("Enter your number: "))          # user input
+print(sum(n))                            # print final result
