e6bab0bf018c162c388000d2b37889c8a85a0061
Abhay Mishra <techabhay06@gmail.com>
2024-10-14 16:42:12 +0530

    Add new commit

--- Python_solutions/function_problem4.py
+++ Python_solutions/function_problem4.py
@@ -9,3 +9,19 @@ def sum(n):          # difine value
 
 n = int(input("Enter your number: "))          # user input
 print(sum(n))                            # print final result
+
+
+# second problem - here we solve the pattern of star with the recusion
+
+# ***
+# **
+# *
+
+def star(n):
+    if(n==0):               # write base condition
+        return 0
+    print("*"* n)            # print star pattern
+    star (n-1)
+
+star(5)                  # print the result
+
