9880ab737aff79d6d5cbfa2714cdf5a2b97c7267
Abhay Mishra <techabhay06@gmail.com>
2024-10-14 13:14:15 +0530

    Add new commit

--- Python_solutions/function_problem2.py
+++ Python_solutions/function_problem2.py
@@ -1,2 +1,18 @@
 
-# 
+#  write a programm to find out greatest number using function.
+
+def greatnum(a, b, c):                        # here we make function with argument
+    if(a>b and a>c):                          # write the logic for great number
+        return a
+    elif(b>a and b>c):
+        return b
+    elif(c>b and c>a):
+        return c
+
+a= 19                          # here we put numbers
+b= 10
+c= 12
+
+print(greatnum(a, b, c))          # here we print the number
+
+
