db1ca3e23fdc1b69d67df5aaade81d8b3d7afd47
Abhay Mishra <techabhay06@gmail.com>
2024-10-21 01:45:53 +0530

    Add new commit

--- Python_solutions/file_IO.py
+++ Python_solutions/file_IO.py
@@ -1,7 +1,19 @@
 
 # Here we write first programm for file I/O
 
+# In this programm we learn how to read data from another file
+
 f = open ("file1.txt")
 data = f.read()
 print(data)
 f.close()
+
+# Now we learn how to write data to another file
+
+st = "Hello, guys I am abhay mishra . welcome to my programming learning. Thankyou"
+
+f = open ("file2.txt", "w")
+
+f.write(st)
+
+f.close
