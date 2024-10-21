834ca5b04a40407a2c3da566670a8c93efe82d20
Abhay Mishra <techabhay06@gmail.com>
2024-10-22 00:19:09 +0530

    Add new commit

--- Python_solutions/file_IO.py
+++ Python_solutions/file_IO.py
@@ -18,5 +18,3 @@ f.write(st)
 
 f.close
 
-
-
--- Python_solutions/file_IO_problem1.py
+++ Python_solutions/file_IO_problem1.py
@@ -1,6 +1,14 @@
 
+# We will learn append mode in file IO
+
+
+
+
+
+
 
 # Here we learn with statement in file IO  
 
 
-
+with open ("file1.txt") as f:
+    print(f.read())
