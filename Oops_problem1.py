5bfa7c143cc574b13785d7c108a9823ef064e621
Abhay Mishra <techabhay06@gmail.com>
2024-10-22 16:56:15 +0530

    Add new commit

--- Oops_problem1.py
+++ Oops_problem1.py
@@ -1,26 +1,56 @@
 
 # Here we learn about Oops concept 
 
-class employee:
-    language = "Java, Python"
-    Salary = "100000"
+# class employee:
+#     language = "Java, Python"
+#     Salary = "100000"
 
-    def get(self):
-        print(f"Here is information: {self.language}")
-        print(f"Here is Salary: {self.Salary}")
+#     def get(self):
+#         print(f"Here is information: {self.language}")
+#         print(f"Here is Salary: {self.Salary}")
 
 
-emp = employee()
-print(emp.language, emp.Salary)
+# emp = employee()
+# print(emp.language, emp.Salary)
 
-emp.get()
+# emp.get()
 
 
 # method of   @staticmethod
 
+# class employee:
+#     language = "Java, Python"
+#     Salary = "100000"
+
+    
+#     def get(self):
+#         print(f"Here is information: {self.language}")
+#         print(f"Here is Salary: {self.Salary}")
+
+#     @staticmethod
+#     def greet(self):
+#         print("Hello, Good to see you here")
+
+
+# emp = employee()
+# print(emp.language, emp.Salary)
+
+# emp.get()
+# emp.greet()
+
+# end here   @staticmethod
+
+
+# we Learn dunder method here 
+# dunder method always call outomaticlay
+
 class employee:
     language = "Java, Python"
     Salary = "100000"
+
+
+    def __init__(self):              # this called dunder method 
+        print("Hello Everyone- ")
 
     
     def get(self):
@@ -28,7 +58,7 @@ class employee:
         print(f"Here is Salary: {self.Salary}")
 
     @staticmethod
-    def greet(pr):
+    def greet(self):
         print("Hello, Good to see you here")
 
 
@@ -38,7 +68,7 @@ print(emp.language, emp.Salary)
 emp.get()
 emp.greet()
 
-# end here   @staticmethod
+
 
 
 
