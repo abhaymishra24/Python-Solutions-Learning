1fb970c32e215b812d30a90e3ef7e468a5d19c57
Abhay Mishra <techabhay06@gmail.com>
2024-10-22 16:13:40 +0530

    Add new commit

--- Oops_problem1.py
+++ Oops_problem1.py
@@ -15,3 +15,32 @@ print(emp.language, emp.Salary)
 
 emp.get()
 
+
+# method of   @staticmethod
+
+class employee:
+    language = "Java, Python"
+    Salary = "100000"
+
+    
+    def get(self):
+        print(f"Here is information: {self.language}")
+        print(f"Here is Salary: {self.Salary}")
+
+    @staticmethod
+    def greet(pr):
+        print("Hello, Good to see you here")
+
+
+emp = employee()
+print(emp.language, emp.Salary)
+
+emp.get()
+emp.greet()
+
+# end here   @staticmethod
+
+
+
+
+
