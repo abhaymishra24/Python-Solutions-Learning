9b8b1508679aa485cf858f571320200b841369e3
Abhay Mishra <techabhay06@gmail.com>
2024-11-23 00:23:03 +0530

    Add new commit

--- Python_fundamental_solutions/function_problem6.py
+++ Python_fundamental_solutions/function_problem6.py
@@ -5,10 +5,7 @@ def number():
     
     n = int(input("Enter your number: "))
     
-    for i in range(n):
-        
-        if n==10:
-            break
-        print(n*2)      
-number()
+    
+
+
         
