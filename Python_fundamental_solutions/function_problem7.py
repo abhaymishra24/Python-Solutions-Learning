d8bbdd6b377f248f27732ba9f90cb2dbfd369926
Abhay Mishra <techabhay06@gmail.com>
2024-11-23 23:24:09 +0530

    Add new commit

--- Python_fundamental_solutions/function_problem7.py
+++ Python_fundamental_solutions/function_problem7.py
@@ -1,1 +1,17 @@
 
+
+def number():
+    
+    n = int(input("Enter your number: "))
+    
+    b = n*2
+    
+    if b == 10:
+        print("It's ten number")
+        print(b)
+    else:
+        print("It's not number")
+        print(b)
+        
+number()
+
