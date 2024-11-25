4782a15691021ef2b84048ad58b9395b40f4296f
Abhay Mishra <techabhay06@gmail.com>
2024-11-25 23:13:40 +0530

    Add new commit

--- Python_fundamental_solutions/function_problem6.py
+++ Python_fundamental_solutions/function_problem6.py
@@ -1,19 +1,26 @@
 
 ## here programm of function - 
 
-def number():
-    
-    n = int(input("Enter your number: "))
-    
-    b = n*2
-    
-    if b == 10:
-        print("It's ten number")
-        print(b)
-    else:
-        print("It's not number")
-        print(b)
+class talk:
+    def number():
+        n = int(input("Enter your number: "))
         
-number()
+        b = n*2
+        
+        
+    
+        if b == 10:
+          print("It's ten number")
+          print(b)
+        else:
+          print("It's not number")
+          print(b)
+        
+    number()
+
+    
+     
+    
+ 
        
        
