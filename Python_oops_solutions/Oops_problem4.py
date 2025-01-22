4b6a0ce91e19efc708d5b231f85f61342f61f76e
Abhay Mishra <techabhay06@gmail.com>
2025-01-22 16:47:28 +0530

    Add new commit

--- Python_oops_solutions/Oops_problem4.py
+++ Python_oops_solutions/Oops_problem4.py
@@ -16,3 +16,23 @@
 # print(score())       
         
         
+def score(x, y, t):
+    results = []
+    
+    for i in range(t):
+        target_score = x - y
+        results.append(target_score)
+    
+    return results
+
+# Input for x and y
+x = int(input("Enter the value for x: "))
+y = int(input("Enter the value for y: "))
+
+# Input for number of test cases
+t = int(input("Enter the number of test cases: "))
+
+# Call the score function and print results
+scores = score(x, y, t)
+for result in scores:
+    print(result)
