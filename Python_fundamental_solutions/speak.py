283163fa2a95f429ca440975221e01ae1dd05d54
Abhay Mishra <techabhay06@gmail.com>
2025-04-13 23:44:33 +0530

    Add new commit

--- Python_fundamental_solutions/speak.py
+++ Python_fundamental_solutions/speak.py
@@ -18,7 +18,7 @@ voices = engine.getProperty('voices')       #getting details of current voice
 #engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
 engine.setProperty('voice', voices[0].id)   #changing index, changes voices. 1 for female
 
-engine.say("")
+engine.say("Astha pagal ladki hai")
 engine.runAndWait()
 engine.stop()
 
