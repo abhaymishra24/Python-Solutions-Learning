7c324b6feddb10de1d79535927b75a52b5698a99
Abhay Mishra <techabhay06@gmail.com>
2025-04-19 11:38:35 +0530

    Add new commit

--- Python_oops_solutions/Oops_problem5.py
+++ Python_oops_solutions/Oops_problem5.py
@@ -1,12 +1,12 @@
 
-# def number():
+def number():
     
-#     a = int(input())
-#     b = a*2
-#     c = b*4
-#     print(c)
+    a = int(input())
+    b = a*2
+    c = b*4
+    print(c)
 
-# n = number()
+n = number()
 
 # now write oops code - 
 
@@ -21,6 +21,10 @@ class number:
         
         b = int(input())
         print(b*b)
+        
+    def divide(self):
+        c = int(input())
+        print(c/c)
         
 t = number()
 t.sum()
