464ecf2cb38b28f02d72e689d9c7f12f4d83f471
Abhay Mishra <techabhay06@gmail.com>
2025-04-21 22:08:33 +0530

    Add new commit

---
+++ Python_oops_solutions/Oops_problem6.py
@@ -0,0 +1,15 @@
+
+# here we learn Oops programming with function - 
+
+def lazy():
+    
+    n = int(input())
+    
+    if n > 7:
+        print("the boy is lazy. He wakeup too late. Its not good thing.")
+        print("Try to early wakeup.")
+    
+    else:
+        print("Good boy.")    
+        
+L=lazy()
