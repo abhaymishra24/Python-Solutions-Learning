9db33c7013ec582ac14620bce99a85f2e7d6d177
Abhay Mishra <techabhay06@gmail.com>
2025-04-26 13:38:22 +0530

    Add new commit

--- Python_oops_solutions/Oops_problem9.py
+++ Python_oops_solutions/Oops_problem9.py
@@ -1,3 +1,17 @@
 
 # here we write oops code for practice - 
 
+class book:
+    
+    def __init__(self, name, author, price):
+        self.name = name
+        self.author = author
+        self.price = price
+        
+    def display(self):
+        print(f"Book Name: {self.name}")
+        print(f"Author Name: {self.author}")
+        print(f"Price: {self.price}")
+        
+b = book("Python Programming", "John Doe", 29.99)
+b.display() 
