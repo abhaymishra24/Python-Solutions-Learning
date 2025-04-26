96ef4d92b476315954529c7d396e92b2c150833e
Abhay Mishra <techabhay06@gmail.com>
2025-04-26 11:03:01 +0530

    Add new commit

--- Python_oops_solutions/Oops_problem6.py
+++ Python_oops_solutions/Oops_problem6.py
@@ -1,71 +1,71 @@
 
-# # here we learn Oops programming with function - 
+# here we learn Oops programming with function - 
 
-# def lazy():
+def lazy():
     
-#     n = int(input())
+    n = int(input())
     
-#     if n > 7:
-#         print("the boy is lazy. He wakeup too late. Its not good thing.")
-#         print("Try to early wakeup.")
+    if n > 7:
+        print("the boy is lazy. He wakeup too late. Its not good thing.")
+        print("Try to early wakeup.")
     
-#     else:
-#         print("Good boy.")    
+    else:
+        print("Good boy.")    
         
-# L=lazy()
+L=lazy()
 
-# # next one - 
+# next one - 
 
-# class India:
+class India:
     
-#     def tech(self):
+    def tech(self):
         
-#         n = int(input())
-#         if n == "Tech Development":
-#             print("India will grow.")
+        n = int(input())
+        if n == "Tech Development":
+            print("India will grow.")
             
-#         else:
-#             print("Depand on U.S, and its not good thing.")
+        else:
+            print("Depand on U.S, and its not good thing.")
             
-# In = India()
-# In.tech()  
+In = India()
+In.tech()  
 
-# # a Oops based code -       
+# a Oops based code -       
 
-# class Work:
+class Work:
     
-#     def coding(self):
+    def coding(self):
         
-#         n = int(input())
-#         if n >= 2:
-#             print("Stop coding, do other thing. Just relex.")
+        n = int(input())
+        if n >= 2:
+            print("Stop coding, do other thing. Just relex.")
                
-#         else:
-#             print("Do code , if you did not feel bore.")
+        else:
+            print("Do code , if you did not feel bore.")
             
-#     def writting(self):
+    def writting(self):
         
-#         t = int(input())
-#         if t >= 3:
-#             print("Stop writting, do other thing. Just relex.")
+        t = int(input())
+        if t >= 3:
+            print("Stop writting, do other thing. Just relex.")
             
-#         else:
-#             print("Do writting, if you did not feel bore")
+        else:
+            print("Do writting, if you did not feel bore")
             
-#     def project(self):
+    def project(self):
         
-#         n = int(input())
-#         if n >= 3:
-#             print("stop doing project. just relex.")
+        n = int(input())
+        if n >= 3:
+            print("stop doing project. just relex.")
             
-#         else:
-#             print("Do project and think about project, if you did not feel bore.")
+        else:
+            print("Do project and think about project, if you did not feel bore.")
             
             
-# w = Work()
-# w.coding()
-# w.project()
-# w.writting()
+w = Work()
+w.coding()
+w.project()
+w.writting()
 
 # write other programming code - 
 
