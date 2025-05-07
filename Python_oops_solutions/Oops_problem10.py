d8e57c58351f9d45bf24cfbac25b551ee6ecf1b9
Abhay Mishra <techabhay06@gmail.com>
2025-05-07 14:25:15 +0530

    Add new commit

--- Python_oops_solutions/Oops_problem10.py
+++ Python_oops_solutions/Oops_problem10.py
@@ -54,15 +54,9 @@
 # t.night()
 
 
-def number(self, n, m, k):
-    
-    self.n = n
-    self.m = m
-    self.k = k
-    
-    print(f"Money from diferent account:{n+m+k}rupees.")
+def number(n, m, k):
+    print(f"Money from diferent account:{n+m+k} rupees.")
     print(f"distribute money in other bussiness: {(n+m+k)//10}")   
     
 number(1000, 2000, 3000)
-number(1000, 2000, 3000)
-number(1000, 2000, 3000)    
+  
