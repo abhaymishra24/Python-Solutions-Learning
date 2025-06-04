73dd2fab99493e16056e83dee1f91cfa9847d534
Abhay Mishra <techabhay06@gmail.com>
2025-06-04 18:59:00 +0530

    Add new commit

--- Python_fundamental_solutions/set_and_dict_problem.py
+++ Python_fundamental_solutions/set_and_dict_problem.py
@@ -2,3 +2,19 @@
 # here we solve set and dict problem - 
 
 # Basics - 
+
+# this is a set code -
+
+t = {1,2,3,4,4,5,4,6,6,0,"Abhay","mis", 'co'}
+
+print(t)
+
+# this is a dict code - 
+
+info = {
+    "name" : "Dileep",
+    "number" : 84857843,
+    "age" : 32
+}
+
+print(info["name"])
