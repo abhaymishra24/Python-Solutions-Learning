5dbdae8ab48b352595e6d6ad072332f8666118ed
Abhay Mishra <techabhay06@gmail.com>
2025-06-06 09:50:34 +0530

    Add new commit

--- Python_oops_solutions/Oops_problem8.py
+++ Python_oops_solutions/Oops_problem8.py
@@ -13,19 +13,19 @@
 
 # Example 1:
 
-Input: nums = [3,2,4], modulo = 2, k = 1
-Output: 3
-Explanation: In this example the interesting subarrays are: 
-The subarray nums[0..0] which is [3]. 
-- There is only one index, i = 0, in the range [0, 0] that satisfies nums[i] % modulo == k. 
-- Hence, cnt = 1 and cnt % modulo == k.  
-The subarray nums[0..1] which is [3,2].
-- There is only one index, i = 0, in the range [0, 1] that satisfies nums[i] % modulo == k.  
-- Hence, cnt = 1 and cnt % modulo == k.
-The subarray nums[0..2] which is [3,2,4]. 
-- There is only one index, i = 0, in the range [0, 2] that satisfies nums[i] % modulo == k. 
-- Hence, cnt = 1 and cnt % modulo == k. 
-It can be shown that there are no other interesting subarrays. So, the answer is 3.
+# Input: nums = [3,2,4], modulo = 2, k = 1
+# Output: 3
+# Explanation: In this example the interesting subarrays are: 
+# The subarray nums[0..0] which is [3]. 
+# - There is only one index, i = 0, in the range [0, 0] that satisfies nums[i] % modulo == k. 
+# - Hence, cnt = 1 and cnt % modulo == k.  
+# The subarray nums[0..1] which is [3,2].
+# - There is only one index, i = 0, in the range [0, 1] that satisfies nums[i] % modulo == k.  
+# - Hence, cnt = 1 and cnt % modulo == k.
+# The subarray nums[0..2] which is [3,2,4]. 
+# - There is only one index, i = 0, in the range [0, 2] that satisfies nums[i] % modulo == k. 
+# - Hence, cnt = 1 and cnt % modulo == k. 
+# It can be shown that there are no other interesting subarrays. So, the answer is 3.
 
 class Solution(object):
     def countInterestingSubarrays(self, nums, modulo, k):
