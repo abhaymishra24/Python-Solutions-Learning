e6312182c7a4d5ed09658d3038ad349f39e416cc
Abhay Mishra <techabhay06@gmail.com>
2025-06-25 13:57:23 +0530

    Add new commit

--- Python_oops_solutions/Oops_problem12.py
+++ Python_oops_solutions/Oops_problem12.py
@@ -1,10 +1,33 @@
 
-def program():
+class Account:
     
-    n = int(input())
+    def __init__(self, bal, acc):
+        
+        self.balacne = bal
+        self.account_no = acc
+        
+    # here we write fuction for debit -
     
-    for i in range(n):
+    def debit(self, amount):
         
-        print(n*n)
+        self.balacne -= amount
+        print("Debited from your account:", amount,"Rupees.")
         
-program()
+    # here we write function for credit -
+    
+    def credit(self, amount):
+        
+        self.balacne += amount
+        print("Credited in your account:", amount,"Rupees.")
+        
+    # here we write method for finale balance -
+    
+    def bal(self):
+        print("Your current balance is:", self.balacne, "Rupees.")
+        
+
+acc1 = Account(20000, 92846624)
+acc1.credit(40000)
+acc1.debit(15000)
+acc1.bal()
+        
