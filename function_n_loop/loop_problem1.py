5b0d7a7fe395d2e3e9281d2af8a8e6c40ee0a808
Abhay Mishra <techabhay06@gmail.com>
2025-07-01 12:51:01 +0530

    Add enw commit

--- function_n_loop/loop_problem1.py
+++ function_n_loop/loop_problem1.py
@@ -1,21 +1,36 @@
 
-# here, we solve loops problems 
+# # here, we solve loops problems 
 
-# this problem solve by for loop
+# # this problem solve by for loop
 
-n = int(input("Enter number: "))
+# n = int(input("Enter number: "))
 
-for i in range(1, 11):
+# for i in range(1, 11):
 
-    print(f"{n} X {i}= {n * i}")
+#     print(f"{n} X {i}= {n * i}")
 
-# same problem solve by while loops
+# # same problem solve by while loops
 
-n = int(input("Enter your number: "))
+# n = int(input("Enter your number: "))
 
-i = 1
+# i = 1
 
-while(i<11):
-    print(f"{n} X {i}= {n*i}")
-    i = i+1
+# while(i<11):
+#     print(f"{n} X {i}= {n*i}")
+#     i = i+1
     
+    
+# here print 1 to 100 number usring while and for loop - 
+
+# first in while loop - 
+
+print("Start from 1.")
+nums = 1
+
+while nums <= 100:
+    print(nums)
+    nums += 1
+     
+   
+print("End at 100.") 
+print("This is the number from 1 to 100.")
