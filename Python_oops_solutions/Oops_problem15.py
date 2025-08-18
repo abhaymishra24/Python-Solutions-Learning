afef590e8badcb45cc6da604295341353643ce77
Abhay Mishra <techabhay06@gmail.com>
2025-08-18 19:41:22 +0530

    Add new commit

---
+++ Python_oops_solutions/Oops_problem15.py
@@ -0,0 +1,20 @@
+
+class Mamber:
+    
+    def __init__(self, Name, Age, Gmail, Phone, College, Department):
+        
+        self.Name = Name
+        self.Age = Age
+        self.Gmail = Gmail
+        self.Phone = Phone
+        self.College = College
+        self.Department = Department
+        
+        def store(self):
+            print("Name:", self.Name)
+            print("Gmail:", self.Gmail)
+            print("College:", self.College)
+            print("Phone:", self.Phone)
+            print("Department:", self.Department)
+            
+            
