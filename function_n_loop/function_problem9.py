e132521255f64458a14bda92b0fbfa5a71e63449
Abhay Mishra <techabhay06@gmail.com>
2025-12-05 17:13:39 +0530

    Add new commit

--- function_n_loop/function_problem9.py
+++ function_n_loop/function_problem9.py
@@ -1,2 +1,16 @@
 
 def number():
+    x = 0
+    y = 0
+    a = 64
+    b = 16
+    
+    add = ((x+y)+(x-y)) = (a+b)
+    print(add)
+    
+    x = add
+    (x+y) = a
+    y = a-x
+    print(y) 
+    
+    
