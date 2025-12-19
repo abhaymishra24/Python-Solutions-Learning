47a6267b3c1192368ba8e78a3ca55862d9d76362
Abhay Mishra <techabhay06@gmail.com>
2025-12-19 11:54:04 +0530

    Add new commit

--- Python_fundamental_solutions/reverse_withoutreversefunction1.py
+++ Python_fundamental_solutions/reverse_withoutreversefunction1.py
@@ -1,1 +1,13 @@
 
+# here wtire a program to reverse a string without using inbuilt reverse function - 
+
+def reverse_string(s):
+    reversed_s = ""
+    for char in s:
+        reversed_s = char + reversed_s
+    return reversed_s   
+
+input_string = "Hello, World!"
+reversed_string = reverse_string(input_string)
+# print("Reversed string:", reversed_string)
+
