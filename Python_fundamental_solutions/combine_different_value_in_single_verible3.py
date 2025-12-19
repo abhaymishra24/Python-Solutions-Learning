fc9712916802488cb8afaad17bdc3d1b19721be0
Abhay Mishra <techabhay06@gmail.com>
2025-12-19 12:05:51 +0530

    Add ne wcommit

--- Python_fundamental_solutions/combine_different_value_in_single_verible3.py
+++ Python_fundamental_solutions/combine_different_value_in_single_verible3.py
@@ -1,1 +1,15 @@
 
+# write a programm to combine different value in single verible -
+# example - [[1,2], [3,4]] = [1,2,3,4]
+
+def combine_values(nested_list):
+    combined_list = []
+    for sublist in nested_list:
+        for item in sublist:
+            combined_list.append(item)
+    return combined_list
+
+# Example usage
+nested_list = [[1, 2], [3, 4]]  
+result = combine_values(nested_list)
+print("Combined list:", result)  # Output: Combined list: [1, 2, 3, 4]
